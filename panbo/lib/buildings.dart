9827559ffd025a0dbdbab93882bada5c018426ea
Federico Ernesto Bulfari <federicoernesto.bulfari@mail.polimi.it>
2026-09-18 19:22:49 +0200

    Refactor buildings and troops enums: reorder items and adjust construction costs for balance

--- panbo/lib/buildings.dart
+++ panbo/lib/buildings.dart
@@ -3,21 +3,21 @@ import 'package:panbo/resources.dart';
 import 'game_model.dart';
 
 enum Buildings {
-  arsenal(GameItem(name: 'Arsenale',icon: '🏰',constructionCost:{Resources.wood:10, Resources.stone:10, Resources.iron:1}, capacity: 250, reqBuilding:{warehouse: true})),
   barracks(GameItem(name: 'Caserma', icon: '🛡️', constructionCost:{Resources.wood:10, Resources.stone:10})),
-  bifurcation(GameItem(name: 'Bivio', icon: '🛣️', constructionCost:{Resources.stone:5}, reqBuilding:{road: false}, turnCost: {})),
-  bridge(GameItem(name: 'Ponte', icon: '🌉', constructionCost:{Resources.stone:5}, turnCost: {})),
-  cityWall(GameItem(name: 'Mura della Città', icon: '城墙', constructionCost:{Resources.stone:20, Resources.iron:2}, reqBuilding:{wall: true}, turnCost: {})),
+  wall(GameItem(name: 'Muro', icon: '🧱', constructionCost:{Resources.stone:10}, turnCost: {})),
+  cityWall(GameItem(name: 'Mura della Città', icon: '城墙', constructionCost:{Resources.stone:10, Resources.iron:1}, reqBuilding:{wall: true}, turnCost: {})),
   forge(GameItem(name: 'Forgia', icon: '⚒️', constructionCost:{Resources.stone:50})),
   harbor(GameItem(name: 'Porto', icon: '⚓', constructionCost:{Resources.wood:30})),
+  wharf(GameItem(name: 'Molo', icon: '🛳️', constructionCost:{Resources.wood:10},reqBuilding:{harbor: false}, turnCost: {})),
   river(GameItem(name: 'Fiume', icon: '🌊', constructionCost:{Resources.wood:10, Resources.stone:5}, turnCost: {})),
+  bridge(GameItem(name: 'Ponte', icon: '🌉', constructionCost:{Resources.stone:5}, turnCost: {})),
   road(GameItem(name: 'Strada', icon: '🛣️', constructionCost:{Resources.stone:10}, turnCost: {})),
-  storageArea( GameItem(name: 'Magazzini all\'aperto', icon: '📦', capacity: 10)),
+  bifurcation(GameItem(name: 'Bivio', icon: '🛣️', constructionCost:{Resources.stone:5}, reqBuilding:{road: false}, turnCost: {})),
   temple(GameItem(name: 'Tempio', icon: '⛪', constructionCost:{Resources.stone:50})),
-  wall(GameItem(name: 'Muro', icon: '🧱', constructionCost:{Resources.stone:15}, turnCost: {})),
+  storageArea( GameItem(name: 'Magazzini all\'aperto', icon: '📦', capacity: 10)),
   warehouse(GameItem(name: 'Magazzino',icon: '🏭',constructionCost:{Resources.wood:10,Resources.stone:10},capacity: 50)),
-  wharf(GameItem(name: 'Molo', icon: '🛳️', constructionCost:{Resources.wood:10},reqBuilding:{harbor: false}, turnCost: {}));
-
+  arsenal(GameItem(name: 'Arsenale',icon: '🏰',constructionCost:{Resources.wood:10, Resources.stone:10, Resources.iron:1}, capacity: 250, reqBuilding:{warehouse: true})),
+  ;
   final GameItem item;
 
   const Buildings(this.item);
--- panbo/lib/troops.dart
+++ panbo/lib/troops.dart
@@ -11,7 +11,7 @@ enum Troops {
   batteringRam(GameItem(name: 'Arieti', icon: '🔨', turnCost: {Resources.wheat:3}, constructionCost:{Resources.wheat:3, Resources.wood:5, Resources.stone:3}, reqBuilding: {Buildings.barracks: false})),
   batteringRam2(GameItem(name: 'Arieti Pesanti', icon: '🔨', turnCost: {Resources.wheat:3}, constructionCost:{Resources.wheat:3, Resources.wood:5, Resources.iron:1}, reqBuilding: {Buildings.barracks: false})),
   trebuchet(GameItem(name: 'Trabucchi', icon: '🎯', turnCost: {Resources.wheat:3}, constructionCost:{Resources.wheat:3, Resources.wood:10}, reqBuilding: {Buildings.barracks: false})),
-  trebuchet2(GameItem(name: 'Sfonda Muro', icon: '🎯', turnCost: {Resources.wheat:3}, constructionCost:{Resources.wheat:3, Resources.wood:20, Resources.iron:1}, reqBuilding: {Buildings.barracks: false})),
+  trebuchet2(GameItem(name: 'Sfonda Muro', icon: '🎯', turnCost: {Resources.wheat:3}, constructionCost:{Resources.wheat:3, Resources.wood:20, Resources.iron:2}, reqBuilding: {Buildings.barracks: false})),
   caravan(GameItem(name: 'Carovane', icon: '🐫', turnCost: {Resources.wheat:3}, constructionCost:{Resources.wood:10}, reqTerritory: {Territories.horse: true}, reqBuilding: {Buildings.barracks: false},capacity: 50)),
   caravan2(GameItem(name: 'Carovane Blindate', icon: '🐫', turnCost: {Resources.wheat:5}, constructionCost:{Resources.wood:20, Resources.iron:1}, reqTerritory: {Territories.horse: true}, reqBuilding: {Buildings.barracks: false},capacity: 250));
 
