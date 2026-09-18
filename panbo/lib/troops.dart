02a928e36a969816777254044e0f3dccccd2d138
Federico Ernesto Bulfari <federicoernesto.bulfari@mail.polimi.it>
2026-09-18 09:13:24 +0200

    minor fix

--- panbo/lib/troops.dart
+++ panbo/lib/troops.dart
@@ -3,63 +3,12 @@ import 'package:panbo/buildings.dart';
 import 'game_model.dart';
 
 enum Troops {
-  brigands(
-    GameItem(
-      name: 'Briganti',
-      icon: '🗡️',
-      cost: 1,
-      wheatCost: 1,
-      woodCost: 3,
-      stoneCost: 1,
-      requirement: Buildings.barracks
-    ),
-  ),
-  knights(
-    GameItem(
-      name: 'Cavalieri',
-      icon: '🐴',
-      cost: 2,
-      wheatCost: 1,
-      woodCost: 3,
-      horseCost: 1,
-      requirement: Buildings.barracks
-    ),
-  ),
-  soldiers(
-    GameItem(
-      name: 'Soldati',
-      icon: '⚔️',
-      cost: 1,
-      wheatCost: 1,
-      woodCost: 3,
-      ironCost: 1,
-      requirement: Buildings.barracks
-    ),
-  ),
-  batteringRam(
-    GameItem(
-      name: 'Arieti',
-      icon: '🔨',
-      cost: 3,
-      wheatCost: 3,
-      woodCost: 5,
-      stoneCost: 3,
-      requirement: Buildings.barracks
-    ),
-  ),
-  trebuchet(
-    GameItem(
-      name: 'Trabucchi',
-      icon: '🎯',
-      cost: 3,
-      wheatCost: 3,
-      woodCost: 10,
-      requirement: Buildings.barracks
-    ),
-  ),
-  caravan(
-    GameItem(name: 'Carovane', icon: '🐫', cost: 3, woodCost: 10, horseCost: 1, requirement: Buildings.barracks),
-  );
+  brigands(GameItem(name: 'Briganti', icon: '🗡️', cost: 1, wheatCost: 1, woodCost: 3, stoneCost: 1, requirement: Buildings.barracks),),
+  knights(GameItem( name: 'Cavalieri', icon: '🐴', cost: 2, wheatCost: 1, woodCost: 3, horseCost: 1, requirement: Buildings.barracks),),
+  soldiers(GameItem(name: 'Soldati', icon: '⚔️', cost: 1, wheatCost: 1, woodCost: 3, ironCost: 1, requirement: Buildings.barracks),),
+  batteringRam(GameItem(name: 'Arieti', icon: '🔨', cost: 3, wheatCost: 3, woodCost: 5, stoneCost: 3, requirement: Buildings.barracks),),
+  trebuchet(GameItem(name: 'Trabucchi', icon: '🎯', cost: 3, wheatCost: 3, woodCost: 10, requirement: Buildings.barracks),),
+  caravan(GameItem(name: 'Carovane', icon: '🐫', cost: 3, woodCost: 10, horseCost: 1, requirement: Buildings.barracks),);
 
   final GameItem item;
 
